287c821b2106c841a05fe5a4badd58fc4339e615
bdnmarshall@gmail.com <aMQ7eCWE>
2021-07-09 23:31:06 +1000

    getting code back to where it was

--- bot/beltbot.py
+++ bot/beltbot.py
@@ -157,10 +157,10 @@ async def approval_handler(ctx, request_id, *reason):
 
     message = (
         f"{member.mention}, {ctx.author.mention} has reviewed and approved your request. "
-        f"Congrats on your {role_name}! \n"
+        f"Congrats on your {role_name}!"
     )
     if reason:
-        message += " ".join(reason_part for reason_part in reason)
+        message += "\nNotes: ".join(reason_part for reason_part in reason)
     if flair_text:
         message += flair_text
 
@@ -210,11 +210,14 @@ async def rejection_handler(ctx, request_id, *reason):
     await delete_request(request_id)
 
     await update_stats("belts_to_the_glue_factory")
-    
 
 @BOT.command(name="delete")
 @requires_role("Mods")
 async def delete_handler(ctx, request_id, *reason):
+    if ctx.message.channel.name != "belt-requests" or "bot-spam":
+        await ctx.send("Only available in #belt-requests or #bot-spam.")
+        return
+
     request = await get_request(request_id)
 
     if request is None:
@@ -225,16 +228,18 @@ async def delete_handler(ctx, request_id, *reason):
         await ctx.send("You need to provide a reason!")
         return
 
-    belt_requests_channel = get_channel_by_name(ctx, "belt-requests")
+    delete_request_channel = get_channel_by_name(ctx, ctx.message.channel.name)
 
-    await belt_requests_channel.send(
+    await delete_request(request_id)
+    
+    await delete_request_channel.send(
         (
             f"{ctx.author.mention} has deleted the following request: {request_id}\n"
             f"\nNotes: {' '.join(reason_part for reason_part in reason)}"
         )
     )
 
-    await delete_request(request_id)
+
 
 
 @BOT.command(name="moreinfo")
